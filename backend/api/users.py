398294184aed3774c1d62ee0cd38400ebae791e9
fearsd <rifatfazlutdinov@gmail.com>
2023-05-28 05:18:46 +0300

    404 hanglig in /users/{id}

--- backend/api/users.py
+++ backend/api/users.py
@@ -2,8 +2,7 @@ from datetime import date
 from typing import List, Optional
 
 from sqlalchemy.orm import Session
-from sqlmodel import select
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel
 
 from db.db import get_db
@@ -25,6 +24,15 @@ class UserListResponse(BaseModel):
     limit: int
     count: int
     users: List[UserAccount]
+
+
+class HTTPError(BaseModel):
+    detail: str
+
+    class Config:
+        schema_extra = {
+            "example": {"detail": "User not found"},
+        }
 
 
 
@@ -51,10 +59,14 @@ def check(fullname: str, birthday: date, db: Session = Depends(get_db)):
     response_model=UserDetail,
     name='Пользователь',
     description='Получение пользователя по id',
+    responses={
+        status.HTTP_404_NOT_FOUND: {'model': HTTPError} 
+    }
 )
 def get_user(id: int, db: Session = Depends(get_db)):
-    # TODO: 404 exception
     user = db.query(UserAccount).get(id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return user
 
 
@@ -73,6 +85,3 @@ def get_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
         'count': count,
         'users': users
     }
-
-
-
